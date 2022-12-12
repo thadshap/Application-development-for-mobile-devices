a3f20aa7dc0847bae6d4770e3dd3d3c0ac899832
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2022-12-12 15:13:28 +0100

    Oving 5

---
+++ Oving_5/app/build/generated/source/buildConfig/debug/no/ntnu/idatt2506/httpandcoroutines/guessinggame/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package no.ntnu.idatt2506.httpandcoroutines.guessinggame;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "no.ntnu.idatt2506.httpandcoroutines.guessinggame";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
